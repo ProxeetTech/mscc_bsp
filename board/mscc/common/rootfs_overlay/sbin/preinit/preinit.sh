7caf3033e7265c49594776b7b4e58e281ae37fc3
cjack <cjack@yandex.ru>
2025-04-06 16:28:53 +0300

    fix preinit

--- board/mscc/common/rootfs_overlay/sbin/preinit/preinit.sh
+++ board/mscc/common/rootfs_overlay/sbin/preinit/preinit.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-CMDLINE=$(cat /proc/cmdline 2>/dev/null)
-
-if echo "$CMDLINE" | grep -q '\brdinit\b'; then
-	mount -t proc proc /proc
-	mount -t sysfs sysfs /sys
-	mount -t devtmpfs devtmpfs /dev
-	chown -R root:root /var/empty
-fi
-
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -t devtmpfs devtmpfs /dev
+mount -t tmpfs -o size=512M tmpfs /tmp
+chown -R root:root /var/empty
 mkdir -p /tmp/update
 mount /dev/mmcblk0p8 /tmp/update
 
